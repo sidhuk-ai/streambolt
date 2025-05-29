72092270c83abef1bee54f03de50819281e9498f
sidhuk-ai <kovisidhardha2004@google.com>
2025-05-29 18:42:19 +0530

    Created main Loading file for (app)/page.tsx

---
+++ apps/frontend/app/(app)/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className='dark:text-white text-black w-full min-h-screen flex justify-center items-center'>
+        Loading...
+    </div>
+  )
+}
+
+export default Loading
