3b6488df125af2ae23162f90bdab3d3340d59cbf
sidhuk-ai <kovisidhardha2004@google.com>
2025-05-30 15:46:50 +0530

    (Feat) Protected the dashboard routes from Unauthorized users.

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,10 +1,20 @@
-export {auth as middleware} from "@/auth"
+import { NextRequest, NextResponse } from "next/server"
+import { auth } from "@/auth"
 
+export async function middleware(req:NextRequest){
+    const session = await auth();
+    const protectedRoute = req.nextUrl.pathname.startsWith("/dashboard");
 
+    if(protectedRoute && !session?.user){
+        return NextResponse.redirect(new URL("/login",req.url))
+    }
+    return NextResponse.next();
+}
 
 export const config = {
     matcher: [
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
         '/(api|trpc)(.*)',
+        "/dashboard/:path*"
     ]
 }
