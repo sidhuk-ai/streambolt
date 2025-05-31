ab733b68a73e43b122c9fee760638df046ba7dc1
sidhuk-ai <kovisidhardha2004@google.com>
2025-05-31 22:14:03 +0530

    Refactored loading component to use Array.from for skeleton generation

--- apps/frontend/app/(app)/browse/loading.tsx
+++ apps/frontend/app/(app)/browse/loading.tsx
@@ -2,42 +2,36 @@ import { Skeleton } from '@/components/ui/skeleton'
 import React from 'react'
 
 const Loading = () => {
-    const skeletonLength = [0,1,2,3,4,5,6,7,8,9,11,12]
   return (
     <div className='flex min-h-screen flex-col'>
         <div className='flex-1'>
             <div className='container py-6 md:px-6'>
                 <div className="flex flex-nowrap gap-2.5 mb-6 overflow-x-hidden justify-center items-center py-2">
-                    {
-                        skeletonLength.map((_,i)=>{
-                            return (
-                                <Skeleton key={i} className='bg-muted-foreground h-7 w-20 rounded-full'/>
-                            )
-                        })
-                        
+                    {Array.from({ length: 12 }).map((_, i) =>{
+                        return (
+                            <Skeleton key={i} className='bg-muted-foreground h-7 w-20 rounded-full'/>
+                        )})
                     }
                 </div>
                 <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full place-content-center">
-                    {
-                        skeletonLength.map((_,i)=>{
-                            return(
-                                <aside key={i}>
-                                    <Skeleton className='bg-muted-foreground rounded-md h-40 w-full'/>
-                                    <div className='flex gap-2 p-2 m-2 flex-row'>
-                                        <Skeleton className='bg-muted-foreground rounded-full h-10 w-10' />
-                                        <div className="flex gap-2 flex-col">
-                                            <Skeleton className='h-6 w-full bg-muted-foreground'/>
-                                            <Skeleton className='h-6 w-full bg-muted-foreground'/>
-                                            <div className='flex gap-1'>
-                                                <Skeleton className='bg-muted-foreground h-4 w-10 rounded-full'/>
-                                                <Skeleton className='bg-muted-foreground h-4 w-10 rounded-full'/>
-                                                <Skeleton className='bg-muted-foreground h-4 w-10 rounded-full'/>
-                                            </div>
+                    {Array.from({length: 12}).map((_,i) => {
+                        return (
+                            <aside key={i}>
+                                <Skeleton className='bg-muted-foreground rounded-md h-40 w-full'/>
+                                <div className='flex gap-2 p-2 m-2 flex-row'>
+                                    <Skeleton className='bg-muted-foreground rounded-full h-10 w-10' />
+                                    <div className="flex gap-2 flex-col">
+                                        <Skeleton className='h-6 w-full bg-muted-foreground'/>
+                                        <Skeleton className='h-6 w-full bg-muted-foreground'/>
+                                        <div className='flex gap-1'>
+                                            <Skeleton className='bg-muted-foreground h-4 w-10 rounded-full'/>
+                                            <Skeleton className='bg-muted-foreground h-4 w-10 rounded-full'/>
+                                            <Skeleton className='bg-muted-foreground h-4 w-10 rounded-full'/>
                                         </div>
                                     </div>
-                                </aside>
-                            )
-                        })
+                                </div>
+                            </aside>
+                        )})
                     }
                 </div>
             </div>
