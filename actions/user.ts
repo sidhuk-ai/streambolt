f6bdbd14bd66143d2ee5ef648c7f36fb01e7d32f
sidhuk-ai <kovisidhardha2004@google.com>
2025-06-07 23:15:38 +0530

    feat: implement user registration and login functionality with error handling

---
+++ actions/user.ts
@@ -0,0 +1,110 @@
+"use server";
+
+import { auth, signIn } from "@/auth";
+import { prisma } from "@/lib/db";
+import { hash } from "bcryptjs";
+import { redirect } from "next/navigation";
+
+export const register = async (formData: FormData) => {
+    const firstName = formData.get('firstName') as string;
+    const lastName = formData.get('lastName') as string;
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirmPassword') as string;
+
+    if(!firstName || !lastName || !email || !password || !confirmPassword) throw new Error("All fields are required.");
+    if(password !== confirmPassword) throw new Error("Password didn't matched.");
+
+    const existingUser = await prisma.user.findUnique({
+        where:{email}
+    })
+    if(existingUser) throw new Error("User already exists. Go back to Login");
+
+    const hashedPassword = await hash(password,10);
+    const fullName = `${firstName} ${lastName}`
+    const username = `user-${Date.now().toString().slice(-5)}`
+    await prisma.user.create({
+        data:{
+            name: fullName,
+            username,
+            email,
+            password: hashedPassword,
+            imageUrl: `https://api.dicebear.com/9.x/initials/svg?seed=${fullName}`,
+            stream: {
+                create:{
+                    name: `${fullName}'s Stream`
+                }
+            }
+        }
+    })
+    redirect('/login')
+}
+
+export const login = async (formData: FormData) => {
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if(!email || !password) throw new Error("All fields are required.");
+
+    await signIn("credentials",{
+        email: email,
+        password: password,
+        redirectTo: '/dashboard'
+    });
+}
+
+export const getSelf = async () => {
+    try {
+        const session = await auth();
+
+        if(!session?.user) throw new Error("Your session was not found. Try Logging in again.");
+
+        return session.user
+    } catch (error) {
+        throw new Error("Error occured while getting your sessions.");
+    }
+}
+
+export const getAllCreators = async () => {
+    const self = await getSelf();
+    const someCreators = await prisma.user.findMany({
+        where:{
+            AND: [
+                {
+                    NOT:{
+                        id: self.id
+                    }
+                },
+                {
+                    // Hamne jise block kiya hai use hata do
+                    blockedBy: {
+                        none:{
+                            blockerId: self.id
+                        }
+                    }
+                },
+                {
+                    // Ham logon ko jisne bhi block kiya hai usko bhi hata do
+                    blocking: {
+                        none:{
+                            blockedId: self.id
+                        }
+                    }
+                }
+            ],
+            
+        },
+        take:5
+    })
+    return someCreators;
+}
+
+export const getUserById = async (id:string) => {
+    const user = await prisma.user.findUnique({
+        where:{id}
+    });
+
+    if(!user) throw new Error("User Doesn't Exists.");
+
+    return user;
+}
