5eaba74201e737d1130725e4f4b8253b2a2bd178
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-06-08 19:32:33 +0530

    improvements: changed the dev port to 3001 and added domain of image URL in NextConfig

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["images.unsplash.com",'via.placeholder.com'], // Add the external image domain here
+  }
 };
 
 export default nextConfig;
