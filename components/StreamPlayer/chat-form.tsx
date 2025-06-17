471136fe4d940dbda62c2d9639784109c316dc5e
sidhuk-ai <kovisidhardha2004@google.com>
2025-06-17 19:55:44 +0530

    feat: update color and background styles for brand consistency in globals.css; refactor ChatForm component for improved accessibility

--- components/StreamPlayer/chat-form.tsx
+++ components/StreamPlayer/chat-form.tsx
@@ -2,7 +2,6 @@
 
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { FormEvent, useMemo, useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
@@ -68,13 +67,19 @@ export default function ChatForm({
             value={value}
             disabled={isDisabled}
             placeholder="Send a message"
-            className={cn("", !isFollowersOnly && "rounded-t-none border-t-0")}
+            className={cn(
+              "",
+              (isFollowersOnly || isDelayed) && "rounded-t-none border-t-0"
+            )}
           />
-          <Button type="submit" size={"sm"} variant={"ghost"} disabled={isDisabled} className="absolute inset-y-0 end-0 flex items-center justify-center pe-3 peer-disabled:opacity-50">
-            <SendHorizonal size={16} className="text-green-400"/>
-          </Button>
+          <button
+            type="submit"
+            disabled={isDisabled}
+            className="absolute inset-y-0 end-0 flex items-center justify-center pe-3 peer-disabled:opacity-50"
+          >
+            <SendHorizonal size={16} className="text-green-400" />
+          </button>
         </div>
-        
       </div>
     </form>
   );
@@ -93,56 +98,51 @@ export const ChatFormSkeleton = () => {
 };
 
 const ChatInfo = ({ isDelayed, isFollowersOnly }: ChatInfoProps) => {
-
   const info = useMemo(() => {
-    if(isFollowersOnly && !isDelayed) {
+    if (isFollowersOnly && !isDelayed) {
       return "Chat is for Followers only.";
     }
 
-    if(isDelayed && !isFollowersOnly) {
+    if (isDelayed && !isFollowersOnly) {
       return "Chats are delayed by 3s.";
     }
 
-    if(isDelayed && isFollowersOnly) {
-      return "Chat is for Followers only, and messages are delayed by 3s."
+    if (isDelayed && isFollowersOnly) {
+      return "Chat is for Followers only, and messages are delayed by 3s.";
     }
 
-    return ""
-  },[isDelayed, isFollowersOnly]);
+    return "";
+  }, [isDelayed, isFollowersOnly]);
 
   const label = useMemo(() => {
-    if(isFollowersOnly && !isDelayed) {
+    if (isFollowersOnly && !isDelayed) {
       return "Followers only";
     }
 
-    if(isDelayed && !isFollowersOnly) {
+    if (isDelayed && !isFollowersOnly) {
       return "Slow mode";
     }
 
-    if(isDelayed && isFollowersOnly) {
-      return "Followers only and Slow mode"
+    if (isDelayed && isFollowersOnly) {
+      return "Followers only and Slow mode";
     }
 
-    return ""
-  },[isDelayed, isFollowersOnly]);
+    return "";
+  }, [isDelayed, isFollowersOnly]);
 
-  if(!isDelayed && !isFollowersOnly) {
+  if (!isDelayed && !isFollowersOnly) {
     return null;
   }
 
   return (
-    <div className="p-2 text-muted-foreground bg-white/5 border-white/10 w-full rounded-t-md flex items-center gap-x-2">
+    <div className="p-2 text-muted-foreground bg-brand/15 border-brand/20 dark:bg-white/5 dark:border-white/10 w-full rounded-t-md flex items-center gap-x-2">
       <Tooltip>
         <TooltipTrigger asChild>
           <Info className="h-4 w-4" />
         </TooltipTrigger>
-        <TooltipContent>
-          {label}
-        </TooltipContent>
+        <TooltipContent>{label}</TooltipContent>
       </Tooltip>
-      <p className="text-xs font-semibold">
-        {info}
-      </p>
+      <p className="text-xs font-semibold">{info}</p>
     </div>
   );
 };
