c48e48bcd6caa1ace44681d57571a617326b02b7
sidhuk-ai <kovisidhardha2004@google.com>
2025-06-17 19:57:24 +0530

    refactor: remove viewerName prop from ChatCommunity and update usage in Chat component; simplify useViewerToken call

--- components/StreamPlayer/chat-community.tsx
+++ components/StreamPlayer/chat-community.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { useParticipants } from "@livekit/components-react";
+import {
+  useLocalParticipant,
+  useParticipants,
+} from "@livekit/components-react";
 import { useMemo, useState, useTransition } from "react";
 import { useDebounceValue } from "usehooks-ts";
 import { Input } from "@/components/ui/input";
@@ -8,13 +11,12 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn, stringToColor } from "@/lib/utils";
 import { onBlock } from "@/actions/block";
 import { toast } from "sonner";
-import { Tooltip, TooltipTrigger } from "../ui/tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 import { Button } from "../ui/button";
 import { MinusCircle } from "lucide-react";
 import { LocalParticipant, RemoteParticipant } from "livekit-client";
 
 interface ChatCommunityProps {
-  viewerName: string;
   hostName: string | null;
   isHidden: boolean;
 }
@@ -26,31 +28,33 @@ interface CommunityItemProps {
 }
 
 export default function ChatCommunity({
-  viewerName,
   hostName,
   isHidden,
 }: ChatCommunityProps) {
   const [value, setValue] = useState("");
   const [debouncedValue] = useDebounceValue<string>(value, 500);
   const participants = useParticipants();
+  const viewer = useLocalParticipant();
 
   const onChange = (newValue: string) => {
     setValue(newValue);
   };
 
   const filteredParticipants = useMemo(() => {
-    const deduped = participants.reduce((acc,participant) => {
+    const deduped = participants.reduce((acc, participant) => {
       const hostAsViewer = `host-${participant.identity}`;
-      if(!acc.some((p) => p.identity === hostAsViewer)) {
-        acc.push(participant)
+      if (!acc.some((p) => p.identity === hostAsViewer)) {
+        acc.push(participant);
       }
       return acc;
     }, [] as (RemoteParticipant | LocalParticipant)[]);
 
     return deduped.filter((participant) => {
-      return participant?.name?.toLowerCase().includes(debouncedValue.toLowerCase())
-    })
-  }, [participants, debouncedValue])
+      return participant?.name
+        ?.toLowerCase()
+        .includes(debouncedValue.toLowerCase());
+    });
+  }, [participants, debouncedValue]);
 
   if (isHidden) {
     return (
@@ -74,7 +78,7 @@ export default function ChatCommunity({
           <CommunityItem
             key={participant.identity}
             hostName={hostName as string}
-            viewerName={viewerName}
+            viewerName={viewer.localParticipant?.name as string}
             participantName={participant.name}
             participantIdentity={participant.identity}
           />
@@ -97,19 +101,28 @@ const CommunityItem = ({
   const isHost = viewerName === hostName;
 
   const handleBlock = () => {
-    if(!participantName || isSelf || !isHost) return null;
+    if (!participantName || isSelf || !isHost) {
+      return null;
+    }
 
     startTransition(() => {
-      onBlock(participantIdentity).then(() => {
-        toast.success(`Blocked ${participantName}`)
-      }).catch(() => {
-        toast.error('Something went wrong.');
-      })
+      onBlock(participantIdentity)
+        .then(() => {
+          toast.success(`Blocked ${participantName}`);
+        })
+        .catch(() => {
+          toast.error("Something went wrong.");
+        });
     });
-  }
+  };
   return (
-    <div className={cn("group flex items-center justify-between w-full p-2 rounded-md text-sm hover:bg-accent", isPending && "opacity-50 pointer-events-none")}>
-      <p className="" style={{color: color}}>
+    <div
+      className={cn(
+        "group flex items-center justify-between w-full p-2 rounded-md text-sm hover:bg-accent",
+        isPending && "opacity-50 pointer-events-none"
+      )}
+    >
+      <p className="" style={{ color: color }}>
         {participantName}
       </p>
       {isHost && !isSelf && (
@@ -119,11 +132,12 @@ const CommunityItem = ({
               variant={"ghost"}
               disabled={isPending}
               onClick={handleBlock}
-              className="h-auto w-auto p-1 opacity-0 group-hover:opacity-100 transition"
+              className="h-auto w-auto opacity-80 group-hover:opacity-100 p-1 transition"
             >
-              <MinusCircle className="h-4 w-4 text-muted-foreground" />
+              <MinusCircle className="h-4 w-4 text-black dark:text-white" />
             </Button>
           </TooltipTrigger>
+          <TooltipContent>Block user</TooltipContent>
         </Tooltip>
       )}
     </div>
