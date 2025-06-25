43a83624f7a59d4edcd1ce2cb8ac3d129fcfdef6
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-06-25 13:58:41 +0530

    refactor: remove unused MarqueeSection and MessageSection components; simplify Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import Hero from "@/components/Hero";
-import MarqueeSection from "@/components/MarqueeSection";
-import MessageSection from "@/components/MessageSection";
-
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="">
       <Hero />
-      <MarqueeSection />
-      <MessageSection />
-      <section className="h-dvh"></section>
     </div>
   );
 }
