5cb604d158f23ec3bcd37ef0fdfb7e3a6218725f
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-06-26 23:03:33 +0530

    feat: enhance Navbar component with improved DrawerMenu and Dropdown functionality; update layout for better responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,9 +10,11 @@ import {
   Clapperboard,
   Globe,
   House,
+  LayoutDashboard,
   LayoutGrid,
   LogInIcon,
   LogOut,
+  LucideIcon,
   Settings,
   User,
   UserPen,
@@ -41,9 +43,62 @@ import {
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
+type DropdownType = {
+  id: number;
+  url: string;
+  title: string;
+  icon: LucideIcon;
+};
+interface DropdownProps {
+  props: DropdownType[];
+  avatar: string | null | undefined;
+}
+
 export default function Navbar() {
   const { data: session, status } = useSession();
   const pathname = usePathname();
+  const authenticatedDrawerItems = [
+    [
+      {
+        id: 1,
+        url: "/dashboard",
+        title: "Dashboard",
+        icon: LayoutDashboard,
+      },
+      {
+        id: 2,
+        url: "/dashboard/my-streams",
+        title: "Your Streams",
+        icon: Clapperboard,
+      },
+      {
+        id: 3,
+        url: "/dashboard/analytics",
+        title: "Analytics",
+        icon: ChartNoAxesCombined,
+      },
+      {
+        id: 4,
+        url: "/dashboard/settings",
+        title: "Settings",
+        icon: Settings,
+      },
+    ],
+    [
+      {
+        id: 1,
+        url: "/login",
+        title: "Login",
+        icon: LogInIcon,
+      },
+      {
+        id: 2,
+        url: "/sign-up",
+        title: "Sign-up",
+        icon: UserRoundPlus,
+      },
+    ],
+  ];
   return (
     <>
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -56,7 +111,7 @@ export default function Navbar() {
               BOLT
             </span>
           </Link>
-          {/* <div className="flex items-center gap-6 md:gap-10">
+          <div className="flex items-center gap-6 md:gap-10">
             <nav className="hidden gap-6 md:flex">
               <Link
                 href="/browse"
@@ -92,7 +147,7 @@ export default function Navbar() {
                 Following
               </Link>
             </nav>
-          </div> */}
+          </div>
           <div className="flex flex-1 items-center space-x-4 justify-end">
             <div className="relative hidden w-full max-w-sm md:flex">
               <SearchBar url="/search" />
@@ -107,7 +162,9 @@ export default function Navbar() {
                 </>
               ) : session?.user ? (
                 <>
-                  <Dropdown session={session} />
+                  <span className="hidden md:block">
+                    <Dropdown session={session} />
+                  </span>
                 </>
               ) : (
                 <>
@@ -120,11 +177,18 @@ export default function Navbar() {
                       <ArrowRight className="h-4 w-4 transition-transform duration-300 group-hover:translate-x-1" />
                     </Link>
                   </Button>
-                  <span className="md:hidden">
-                    <DrawerMenu />
-                  </span>
                 </>
               )}
+              <span className="md:hidden">
+                <DrawerMenu
+                  avatar={session?.user?.image}
+                  props={
+                    session
+                      ? authenticatedDrawerItems[0]
+                      : authenticatedDrawerItems[1]
+                  }
+                />
+              </span>
             </div>
           </div>
         </div>
@@ -134,11 +198,18 @@ export default function Navbar() {
   );
 }
 
-function DrawerMenu() {
+function DrawerMenu({ props, avatar }: DropdownProps) {
   return (
     <Drawer>
       <DrawerTrigger asChild className="cursor-pointer">
-        <User className="h-9 w-9 border rounded-full p-1.5" />
+        {!!avatar ? (
+          <Avatar className="size-9">
+            <AvatarImage src={avatar} />
+            <AvatarFallback>USER</AvatarFallback>
+          </Avatar>
+        ) : (
+          <User className="h-9 w-9 border rounded-full p-1.5" />
+        )}
       </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto w-full max-w-sm">
@@ -147,18 +218,18 @@ function DrawerMenu() {
           </DrawerHeader>
           <div className="p-4 pb-0">
             <div className="flex items-center justify-center space-y-2 mb-4 flex-col">
-              <Link href={"/login"} className="w-full">
-                <Button variant={"outline"} className="w-full">
-                  <LogInIcon />
-                  Login
+              {props.map((item) => (
+                <Button
+                  variant={item.title === "Sign-up" ? "default" : "outline"}
+                  className="w-full"
+                  key={item.id}
+                >
+                  <Link href={item.url} className="flex items-center gap-2.5">
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </Link>
                 </Button>
-              </Link>
-              <Link href={"/sign-up"} className="w-full">
-                <Button className="w-full">
-                  <UserRoundPlus />
-                  Sign Up
-                </Button>
-              </Link>
+              ))}
             </div>
           </div>
         </div>
@@ -221,23 +292,39 @@ function Dropdown({ session }: { session: Session }) {
   );
 }
 
-function BottomNav({ paths } : { paths: string }) {
+function BottomNav({ paths }: { paths: string }) {
   return (
     <div className="flex z-50 fixed bottom-0 md:hidden w-full justify-between items-center p-4 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 border-t">
       <Link href={"/"} className="flex flex-col items-center transition">
-        <House className={cn(paths === '/' && "fill-accent-foreground stroke-accent")} />
+        <House
+          className={cn(
+            paths === "/" && "fill-accent-foreground stroke-accent"
+          )}
+        />
         <span className="text-xs">Home</span>
       </Link>
       <Link href={"/browse"} className="flex flex-col items-center">
-        <Globe  className={cn(paths === '/browse' && "fill-accent-foreground stroke-accent")} />
+        <Globe
+          className={cn(
+            paths === "/browse" && "fill-accent-foreground stroke-accent"
+          )}
+        />
         <span className="text-xs">Browse</span>
       </Link>
       <Link href={"/categories"} className="flex flex-col items-center">
-        <LayoutGrid  className={cn(paths === '/categories' && "fill-accent-foreground stroke-0")} />
+        <LayoutGrid
+          className={cn(
+            paths === "/categories" && "fill-accent-foreground stroke-0"
+          )}
+        />
         <span className="text-xs">Categories</span>
       </Link>
       <Link href={"/following"} className="flex flex-col items-center">
-        <Users className={cn(paths === '/following' && "fill-accent-foreground stroke-0")} />
+        <Users
+          className={cn(
+            paths === "/following" && "fill-accent-foreground stroke-0"
+          )}
+        />
         <span className="text-xs">Following</span>
       </Link>
     </div>
